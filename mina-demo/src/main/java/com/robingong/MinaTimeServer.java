3ffd6909bd3332f9a7613c6c378ce8a42b0a12bf
Robin Gong <i.robingong@gmail.com>
2019-05-04 19:13:13 +0800

    init repo

---
+++ mina-demo/src/main/java/com/robingong/MinaTimeServer.java
@@ -0,0 +1,28 @@
+package com.robingong;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+@Slf4j
+public class MinaTimeServer {
+
+    private static final int PORT = 3360;
+
+    public static void main(String[] args) throws IOException {
+        IoAcceptor acceptor = new NioSocketAcceptor();
+        acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+        acceptor.setHandler(new TimeServerHandler());
+        acceptor.bind(new InetSocketAddress(PORT));
+        log.info("Mina time server is ready");
+    }
+
+}
---
+++ mina-demo/src/main/java/com/robingong/TimeServerHandler.java
@@ -0,0 +1,26 @@
+package com.robingong;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+
+import java.util.Date;
+
+@Slf4j
+public class TimeServerHandler extends IoHandlerAdapter {
+
+    private static final String QUIT_CMD = "quit";
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        String messageStr = message.toString();
+        log.info("Message get: {}", messageStr);
+        if (QUIT_CMD.equalsIgnoreCase(messageStr.trim())) {
+            session.closeNow();
+            return;
+        }
+        Date date = new Date();
+        session.write(date.toString());
+        log.info("Message wrote: {}", date.toString());
+    }
+}
